bda1c81ade0cda8077db444374d8a6fc9937dd12
Ibrahim Hussain <ibrahim.hucyn@live.com>
2019-03-05 10:33:18 +0500

    Set up FK for dbo.Test

--- OutsideSamplesDatabase/Tables/Data/Test.sql
+++ OutsideSamplesDatabase/Tables/Data/Test.sql
@@ -5,6 +5,8 @@
     [SpecimenId] INT NOT NULL, 
     [TransportDetailsId] INT NOT NULL, 
     [RequiredDetails] INT NOT NULL, 
-    [Volume] DECIMAL NOT NULL
+    [Volume] DECIMAL NOT NULL, 
+    CONSTRAINT [FK_Test_Specimen] FOREIGN KEY ([SpecimenId]) REFERENCES [Specimen]([Id]), 
+    CONSTRAINT [FK_Test_TransportDetails] FOREIGN KEY ([TransportDetailsId]) REFERENCES [TransportDetails]([Id])
 
 )
