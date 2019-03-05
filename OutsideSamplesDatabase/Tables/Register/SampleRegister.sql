bd0abb8842ed979f60ebfe1cdc025fcc2dcd6cf7
Ibrahim Hussain <ibrahim.hucyn@live.com>
2019-03-05 10:39:47 +0500

    Added indexes and FK to Sample Register table

--- OutsideSamplesDatabase/Tables/Register/SampleRegister.sql
+++ OutsideSamplesDatabase/Tables/Register/SampleRegister.sql
@@ -4,5 +4,15 @@
     [HospitalNumber] INT NOT NULL, 
     [Age] INT NOT NULL, 
     [SampleId] INT NOT NULL, 
-    [ClinicalHistory] NTEXT NOT NULL
+    [ClinicalHistory] NTEXT NOT NULL, 
+    CONSTRAINT [FK_SampleRegister_Patient] FOREIGN KEY ([HospitalNumber]) REFERENCES [Patient]([HospitalNumber]), 
+    CONSTRAINT [FK_SampleRegister_Sample] FOREIGN KEY ([SampleId]) REFERENCES [Sample]([Id]), 
 )
+
+GO
+
+CREATE INDEX [IX_SampleRegister_HospitalNumber] ON [dbo].[SampleRegister] ([HospitalNumber])
+
+GO
+
+CREATE INDEX [IX_SampleRegister_SampleId] ON [dbo].[SampleRegister] ([SampleId])
